7ce40581b817a93fdb9fb33b9a7da9eb5f8debdd
rcrobot <pcastle@hawk.iit.edu>
2020-03-15 15:09:08 -0500

    update

--- app/src/main/java/com/example/foodfinder/MainActivity.kt
+++ app/src/main/java/com/example/foodfinder/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.foodfinder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
     }
 }
