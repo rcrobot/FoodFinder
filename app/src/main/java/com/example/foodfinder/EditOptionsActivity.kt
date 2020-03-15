eda48d0fb5d3a73e940d15625e1355bc260770a1
rcrobot <pcastle@hawk.iit.edu>
2020-03-14 21:57:52 -0500

    update

---
+++ app/src/main/java/com/example/foodfinder/EditOptionsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditOptionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_options)
+    }
+}
